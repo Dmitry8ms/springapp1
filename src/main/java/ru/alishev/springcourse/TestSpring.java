147b2bfb3bcfd8150ec36341995833373078932b
Dmitry8ms <dshatokhin@yandex.ru>
2023-02-03 22:25:24 +0700

    Scope

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -9,11 +9,16 @@ public class TestSpring {
         );
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer1 == musicPlayer2);
+        musicPlayer1.setVolume(10);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
+        /*musicPlayer.playMusic();
         musicPlayer.playMusicList();
         System.out.println(musicPlayer.getName());
-        System.out.println("Volume: " + musicPlayer.getVolume());
+        System.out.println("Volume: " + musicPlayer.getVolume());*/
         context.close();
     }
 
