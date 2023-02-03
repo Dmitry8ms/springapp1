eb0b9288d7b5754ec0c7eb52772d3fda0e44c289
Dmitry8ms <dshatokhin@yandex.ru>
2023-02-03 16:30:35 +0700

    Dependency Injection part 2

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,11 +1,48 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
     public MusicPlayer(Music music) {
         this.music = music;
     }
+    public MusicPlayer() {}
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusicList(ArrayList musicList) {
+        this.musicList = musicList;
+    }
+    public void playMusicList() {
+        System.out.println("Playing play list:");
+        for (Music music:musicList) {
+            System.out.println(music.getSong());
+        }
     }
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -11,6 +11,9 @@ public class TestSpring {
         //MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        musicPlayer.playMusicList();
+        System.out.println(musicPlayer.getName());
+        System.out.println("Volume: " + musicPlayer.getVolume());
         context.close();
     }
 
